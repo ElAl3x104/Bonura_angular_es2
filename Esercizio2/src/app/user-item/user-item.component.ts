d2658609fd66dd6cb6171596ecbe928a1d4d47f6
ElAl3x104 ! <bonura.alex@iisgalvanimi.edu.it>
2024-11-28 09:55:39 +0000

    angular list

---
+++ Esercizio2/src/app/user-item/user-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  imports: [],
+  templateUrl: './user-item.component.html',
+  styleUrl: './user-item.component.css'
+})
+export class UserItemComponent implements OnInit {
+  @Input() name!: string;
+  
+  constructor() { 
+    
+  }
+  ngOnInit() {
+  }
+}
